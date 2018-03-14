f2a9388fceb687132d5ada983079bc0774436ebf
zhangtusheng <zhangtusheng@ygsoft.com>
2018-03-14 14:15:15 +0800

    hello world

---
+++ helloworld/src/main/java/com/zts/HelloWorldApplication.java
@@ -0,0 +1,18 @@
+package com.zts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
+public class HelloWorldApplication {
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World!";
+    }
+    public static void main(String[] args){
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
+}
